aefed91016b46a7eb8543f7ba533d4460b0f6a96
Jhansi-112 <jhansibukke112@gmail.com>
2024-10-28 17:10:13 +0530

    Updated main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "test-server" {
      inline = ["echo 'wait to start the instance' "]
   }
   tags = {
-     Name = "prod-server"
+     Name = "test-server"
      }
   provisioner "local-exec" {
      command = "echo ${aws_instance.test-server.public_ip} > inventory"
