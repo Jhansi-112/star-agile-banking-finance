92245d0e63689128008600d65390a774c0ad0a94
Jhansi-112 <jhansibukke112@gmail.com>
2025-07-10 22:26:45 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,31 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-05ffe3c48a9991133"
-  instance_type = "t2.micro"
-  key_name = "mykey-project"
+  ami                    = "ami-05ffe3c48a9991133"
+  instance_type          = "t2.micro"
+  key_name               = "mykey-project"
   vpc_security_group_ids = ["sg-0e56c0369e252aaf1"]
+
   connection {
-     type = "ssh"
-     user = "ec2-user"
-     private_key = file("./var/lib/jenkins/workspace/BankingProject/terraform-files/new-project-key.pem")
-     host = self.public_ip
-     }
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("${path.module}/new-project-key.pem") # Make sure this file exists in the same folder
+    host        = self.public_ip
+  }
+
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = [
+      "echo 'wait to start the instance'"
+    ]
   }
+
   tags = {
-     Name = "test-server1"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-files/ansibleplaybook.yml"
-     }
+    Name = "test-server1"
   }
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/BankingProject/terraform-files/ansibleplaybook.yml"
+  }
+}
